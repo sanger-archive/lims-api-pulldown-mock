f7b318a10a6fba9c2cc1678298c45daaf8709c55
Karel <ke4@sanger.ac.uk>
2012-11-26 11:49:51 +0000

    [#39541587] Add mocked transfer_template GET request

--- lib/pulldown/server.rb
+++ lib/pulldown/server.rb
@@ -64,8 +64,7 @@ module Lims
           h["barcode_printers"] = {"actions" => {"first" => "#{path}barcode_printers"}}
           h["state_changes"] = {"actions" => {"first" => "#{path}state_changes", "create" => "#{path}state_changes"}}
           h["transfer_templates"] = {"actions" => {"first" => "#{path}transfer_templates"}}
-          #h["plate_creations"] = {"actions" => {"create" => "#{path}plates"}}
-          h["plate_creations"] = {"actions" => {"create" => "#{path}actions/create_plate", "first" => "#{path}actions/create_plate"}}
+          h["plate_creations"] = {"actions" => {"create" => "#{path}plates"}}
           h.to_json
         end
 
@@ -78,6 +77,119 @@ module Lims
             "first" => "#{path}",
             "read" => "#{path}",
             "last" => "#{path}"}}.to_json
+        end
+        
+        get '/transfer_templates_1_12' do
+          path_preview = request.url
+          path = path_preview - request.env["PATH_INFO"]
+          headers({"Content-Type" => "application/json"})
+          { "transfer_template" => { "actions" => { "create" => "#{path}/actions/plate_transfer",
+                    "preview" => "#{path_preview}/preview",
+                    "read" => "#{path}/actions/plate_transfer"
+                  },
+                "created_at" => "2012-11-20T16:46:14+00:00",
+                "name" => "Transfer columns 1-12",
+                "transfers" => { "A1" => "A1",
+                    "A10" => "A10",
+                    "A11" => "A11",
+                    "A12" => "A12",
+                    "A2" => "A2",
+                    "A3" => "A3",
+                    "A4" => "A4",
+                    "A5" => "A5",
+                    "A6" => "A6",
+                    "A7" => "A7",
+                    "A8" => "A8",
+                    "A9" => "A9",
+                    "B1" => "B1",
+                    "B10" => "B10",
+                    "B11" => "B11",
+                    "B12" => "B12",
+                    "B2" => "B2",
+                    "B3" => "B3",
+                    "B4" => "B4",
+                    "B5" => "B5",
+                    "B6" => "B6",
+                    "B7" => "B7",
+                    "B8" => "B8",
+                    "B9" => "B9",
+                    "C1" => "C1",
+                    "C10" => "C10",
+                    "C11" => "C11",
+                    "C12" => "C12",
+                    "C2" => "C2",
+                    "C3" => "C3",
+                    "C4" => "C4",
+                    "C5" => "C5",
+                    "C6" => "C6",
+                    "C7" => "C7",
+                    "C8" => "C8",
+                    "C9" => "C9",
+                    "D1" => "D1",
+                    "D10" => "D10",
+                    "D11" => "D11",
+                    "D12" => "D12",
+                    "D2" => "D2",
+                    "D3" => "D3",
+                    "D4" => "D4",
+                    "D5" => "D5",
+                    "D6" => "D6",
+                    "D7" => "D7",
+                    "D8" => "D8",
+                    "D9" => "D9",
+                    "E1" => "E1",
+                    "E10" => "E10",
+                    "E11" => "E11",
+                    "E12" => "E12",
+                    "E2" => "E2",
+                    "E3" => "E3",
+                    "E4" => "E4",
+                    "E5" => "E5",
+                    "E6" => "E6",
+                    "E7" => "E7",
+                    "E8" => "E8",
+                    "E9" => "E9",
+                    "F1" => "F1",
+                    "F10" => "F10",
+                    "F11" => "F11",
+                    "F12" => "F12",
+                    "F2" => "F2",
+                    "F3" => "F3",
+                    "F4" => "F4",
+                    "F5" => "F5",
+                    "F6" => "F6",
+                    "F7" => "F7",
+                    "F8" => "F8",
+                    "F9" => "F9",
+                    "G1" => "G1",
+                    "G10" => "G10",
+                    "G11" => "G11",
+                    "G12" => "G12",
+                    "G2" => "G2",
+                    "G3" => "G3",
+                    "G4" => "G4",
+                    "G5" => "G5",
+                    "G6" => "G6",
+                    "G7" => "G7",
+                    "G8" => "G8",
+                    "G9" => "G9",
+                    "H1" => "H1",
+                    "H10" => "H10",
+                    "H11" => "H11",
+                    "H12" => "H12",
+                    "H2" => "H2",
+                    "H3" => "H3",
+                    "H4" => "H4",
+                    "H5" => "H5",
+                    "H6" => "H6",
+                    "H7" => "H7",
+                    "H8" => "H8",
+                    "H9" => "H9"
+                  },
+                "updated_at" => "2012-11-20T16:46:14+00:00",
+                "uuid" => "transfer_templates_1_12"
+              }
+          }.to_json
         end
 
         post '/state_changes' do
@@ -98,16 +210,6 @@ module Lims
           h = JSON.parse(body.first)
           h["plates"].first.to_json
         end
-
-        #get '/pulldown/transfer-columns' do
-        #  path = request.url
-        #  headers({"Content-Type" => "application/json"})
-        #  {"transfer_template" => {"transfers" => {"actions" => {"create" => "#{path}", "first" => "#{path}"}}}}.to_json
-        #end
-
-
-
-
 
         get '/pulldown/find-user' do
           path = request.url
