1bc09f3211a1840cd2ab13b9067eeeaa616b8ca6
Loic <llh1@sanger.ac.uk>
2012-12-12 13:14:57 +0000

    [#41085961] Get the order uuid from plate uuid. Hardcoded value removed.

--- lib/pulldown/resources/order.rb
+++ lib/pulldown/resources/order.rb
@@ -3,21 +3,35 @@ module Lims::Api
     module Order
 
       def order_uuid
-        "6a9ff110-2678-0130-810c-282066132de2"
+        uuid_resources = @context.store.with_session do |session|
+          criteria = {:item => {:uuid => self.uuid}}
+          filter = Lims::Core::Persistence::MultiCriteriaFilter.new(:criteria => criteria)
+          search = Lims::Core::Persistence::Search.new(:description => 'search order',
+                                                       :model => session.send('order').model,
+                                                       :filter => filter)
+          search.call(session).slice(0,9).inject([]) do |m,o| 
+            order_id = session.id_for(o)
+            m << session.uuid_resource[:model_class => 'Order', :key => order_id]
+          end
+        end
+        uuid_resources.empty? ? nil : uuid_resources.first.uuid
       end
 
       def item
-        @order_item ||= @context.store.with_session do |s|
-          order = s[order_uuid]
-          lambda {
-            order.keys.each do |key|
-              if order[key].uuid == self.uuid
-                return OpenStruct.new(:role => key.to_s, :status => order[key].status.to_s)
+        @order_item ||= OpenStruct.new(:role => "mocked", :status => "mocked")
+        begin
+          @order_item = @context.store.with_session do |s|
+            order = s[order_uuid]
+            lambda {
+              order.keys.each do |key|
+                if order[key].uuid == self.uuid
+                  return OpenStruct.new(:role => key.to_s, :status => order[key].status.to_s)
+                end
               end
-            end
-            return OpenStruct.new(:role => "mocked", :status => "mocked")
-          }
-        end.call
+            }
+          end.call
+        rescue
+        end
         @order_item
       end
 
